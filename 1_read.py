a8e28d75e90eca9635387e85de4fc2950f160fe4
Aaron-labo <wxy1294685672.com>
2021-02-16 15:41:17 +0800

    对图片及视频的读取

--- 1_read.py
+++ 1_read.py
@@ -1,13 +1,17 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-cap = cv2.VideoCapture(0)
-print(cap.isOpened())
-while (cap.isOpened()):
+cap = cv2.VideoCapture("F:\\Personal\\Videos\\IDM\\控方证人.ts")
+
+while(cap.isOpened()):
     ret, frame = cap.read()
-    frame = frame[:, ::-1, :]
-    cv2.imshow("cap", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    if ret == True:
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(25) &0xFF == ord('q'):
+            break
+
 cap.release()
 cv2.destroyAllWindows()
+
+
