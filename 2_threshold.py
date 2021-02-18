de315c7f2960c1e9151a74890f4338620e6e25b9
Aaron-labo <wxy1294685672.com>
2021-02-18 11:23:51 +0800

    二值化处理

--- 2_threshold.py
+++ 2_threshold.py
@@ -1,4 +1,5 @@
 import cv2
+import matplotlib.pyplot as plt
 
 img = cv2.imread("E:\\Python\\OpenCV_StudyNotes\\picture\\credit_card1.png")
 
@@ -9,12 +10,22 @@ ret3, thresh3 = cv2.threshold(gray, 127, 255, cv2.THRESH_TRUNC)
 ret4, thresh4 = cv2.threshold(gray, 127, 255, cv2.THRESH_TOZERO)
 ret5, thresh5 = cv2.threshold(gray, 127, 255, cv2.THRESH_TOZERO_INV)
 
-cv2.imshow("image", img)
-cv2.imshow("Binary", thresh1)
-cv2.imshow("Gray Binary", thresh2)
-cv2.imshow("Gray Trunc", thresh3)
-cv2.imshow("Gray Tozero", thresh4)
-cv2.imshow("Gray Tozero_inv", thresh5)
+title = ['image', 'binary', 'gray binary', 'trunc', 'tozero', 'tozero_inv']
+image = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+for i in range(len(image)):
+    plt.subplot(3, 2, i+1)
+    plt.imshow(image[i])
+    plt.title(title[i])
+    plt.xticks([]), plt.yticks([])
+plt.show()
+
+# cv2.imshow("image", img)
+# cv2.imshow("Binary", thresh1)
+# cv2.imshow("Gray Binary", thresh2)
+# cv2.imshow("Gray Trunc", thresh3)
+# cv2.imshow("Gray Tozero", thresh4)
+# cv2.imshow("Gray Tozero_inv", thresh5)
+#
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
