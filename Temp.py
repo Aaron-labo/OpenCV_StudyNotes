a0c5154a21b1869dc856767e0aa3a643fb15df37
Aaron-labo <wxy1294685672.com>
2021-02-18 11:59:47 +0800

    临时文件

--- Temp.py
+++ Temp.py
@@ -1,5 +1,17 @@
-dir = {}
+import cv2
+import numpy as np
 
-dir[0] = 1
-dir[1] = 2
-print(dir)
+img = cv2.imread('picture\\lion.jpeg')
+rows, cols = img.shape[:2]
+
+pts1 = np.float32([[50, 50], [200, 50], [50, 200]])
+pts2 = np.float32([[10, 100], [200, 50], [100, 250]])
+
+M = cv2.getAffineTransform(pts1, pts2)
+
+dst = cv2.warpAffine(img, M, (cols, rows))
+
+cv2.imshow('origin', img)
+cv2.imshow('img', dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
