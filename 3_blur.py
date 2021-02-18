1675895bdd6946f9b109c0813b340f6b522e2a64
Aaron-labo <wxy1294685672.com>
2021-02-18 11:24:26 +0800

    低通滤波，模糊处理

--- 3_blur.py
+++ 3_blur.py
@@ -1,19 +1,31 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-img = cv2.imread("D:\Personal\Pictures\wallpaper\lion.jpeg")
-img = cv2.resize(img, None, fx = 0.8, fy = 0.8)
+img = cv2.imread("E:\\Python\\OpenCV_StudyNotes\\picture\\lena.jpg")
+img = cv2.resize(img, None, fx=2, fy=2)
 imgBlur = cv2.blur(img, (3, 3))
 imgGaussBlur = cv2.GaussianBlur(img, (3, 3), 1)
 imgMedBlur = cv2.medianBlur(img, 3)
+imgBilateral = cv2.bilateralFilter(img, 9, 75, 75)
 
-res = np.hstack([img, imgMedBlur, imgGaussBlur, imgBlur])
-cv2.imshow("All Image", res)
+# image = [img, imgMedBlur, imgGaussBlur, imgBlur, imgBilateral]
+# title = ['original', 'blur', 'guassian', 'median', 'bilateral']
+# length = len(image)
+# for i in range(length):
+#     plt.subplot(length//2+1, 2, i+1)
+#     plt.imshow(image[i], cmap=None)
+#     plt.title(title[i])
+#     plt.xticks([])
+#     plt.yticks([])
+# plt.show()
 
-# cv2.imshow("Original Image", img)
-# cv2.imshow("Blur Image", imgBlur)
-# cv2.imshow("Gaussian Blur Image", imgGaussBlur)
-# cv2.imshow("Median Blur Image", imgMedBlur)
+cv2.imshow("Original Image", img)
+cv2.imshow("Blur Image", imgBlur)
+cv2.imshow("Gaussian Blur Image", imgGaussBlur)
+cv2.imshow("Median Blur Image", imgMedBlur)
+cv2.imshow("Bilateral Blur Image", imgBilateral)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
