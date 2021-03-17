28a5b5a7f6614a6c56ecdf638431b32937ff6a50
Aaron-labo <wxy1294685672.com>
2021-03-17 20:44:39 +0800

    轮廓检测处理

--- 8_contours.py
+++ 8_contours.py
@@ -1,27 +1,19 @@
-import cv2 as cv
+import cv2
 
-img = cv.imread("D:\Personal\Pictures\wallpaper\lion.jpeg")
-img = cv.resize(img, None, fx = 0.8, fy = 0.8)
+#图像预处理（灰度及二值化）
+img = cv2.imread("E:\\Python\\OpenCV_StudyNotes\\picture\\res2.jpg")
+img2 = img.copy()
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)[1]
 
-imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-ret, imgBinary = cv.threshold(imgGray, 127, 255, cv.THRESH_BINARY)
-contours, hierarcy = cv.findContours(imgBinary, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+contours = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
+cv2.drawContours(img2, contours, -1, (0, 0, 255), 1)
+cnt = contours[0]
+x, y, w, h = cv2.boundingRect(cnt)
+cv2.rectangle(img2, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
-#绘制轮廓
-draw_img = img.copy()
-cv.drawContours(draw_img, contours, -1, (0, 0, 255), 1)
-#drawContours会修改原图
-
-#轮廓近似
-cnt = contours[5]
-approx = cv.approxPolyDP(cnt, 0.1, True)
-draw_img2 = img.copy()
-cv.drawContours(draw_img, [approx], -1, (0, 0, 255), 2)
-cv.imshow("Approx Image", draw_img2)
-
-cv.imshow("Original Image", img)
-cv.imshow("Binary Image", imgBinary)
-cv.imshow("Draw Image", draw_img)
-
-cv.waitKey(0)
-cv.destroyAllWindows()
+#输出图像
+cv2.imshow('image', img)
+cv2.imshow('contours', img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
